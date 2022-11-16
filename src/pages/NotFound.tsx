3b83fbaeb66d7d7f2db3702de01af3c60ace39d3
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-11-16 12:44:35 -0500

    fix: update button styling for not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
     <div className="w-full h-full flex flex-col justify-center items-center p-20">
       <h1 className="text-4xl font-bold">404: Page Not Found</h1>
       <button
-        className="border border-black rounded-xl py-2 px-8 mt-6"
+        className="border border-white rounded-xl py-2 px-8 mt-6 hover:bg-white hover:text-[#0b131b] hover:shadow-2xl hover:duration-150"
         onClick={() => navigate("/")}
       >
         Go Back
